5e35ff9568feebb3d58eb1b1879d7e8a4d3b93b2
minecrafting-123 <bright.zheng@thinksign.com>
2025-09-13 00:41:08 -0400

    rm: no database

--- scripts/background.js
+++ scripts/background.js
@@ -1,19 +1,3 @@
-// const OFFSCREEN_URL = 'db/offscreen.html';
-
-// chrome.runtime.onInstalled.addListener(() => ensureOffscreen());
-// chrome.runtime.onStartup.addListener(() => ensureOffscreen());
-
-// async function ensureOffscreen() {
-//   const has = await chrome.offscreen.hasDocument?.();
-//   if (!has) {
-//     await chrome.offscreen.createDocument({
-//       url: OFFSCREEN_URL,
-//       reasons: ['BLOBS'], // any valid reason is fine; we need a long-lived DOM context
-//       justification: 'Keep SQLite WASM and OPFS DB open'
-//     });
-//   }
-// }
-
 chrome.commands.onCommand.addListener(async (command) => {
   if (command !== "toggle-lock") return;
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
